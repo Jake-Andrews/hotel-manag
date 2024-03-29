48b463fc6397932b6f216e9bf997518fc99a2e28
Jake-Andrews <jake.andrews1@ontariotechu.net>
2024-03-28 21:08:23 -0400

    Added spring web for controller web.bind.annotation.Get/Post mappings Added basic GetMapping to controllers

--- src/main/java/com/school/hotelmanagment/hotelmanag/controller/ProvidedServiceController.java
+++ src/main/java/com/school/hotelmanagment/hotelmanag/controller/ProvidedServiceController.java
@@ -1,5 +1,12 @@
 package com.school.hotelmanagment.hotelmanag.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProvidedServiceController {
-    
+    @GetMapping("/services")
+    public String getServicesPage() {
+        return "services";
+    }
 }
--- src/main/java/com/school/hotelmanagment/hotelmanag/controller/RoomController.java
+++ src/main/java/com/school/hotelmanagment/hotelmanag/controller/RoomController.java
@@ -1,5 +1,13 @@
 package com.school.hotelmanagment.hotelmanag.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RoomController {
-    
+    @GetMapping("/rooms")
+    public String getRoomsPage() {
+        return "rooms";
+    }
+
 }
